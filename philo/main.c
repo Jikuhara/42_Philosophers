cf154899842e4ec51b216e405f6fb9cafd07835c
Jikuhara <kei2003730@gmail.com>
2025-07-11 13:30:55 +0900

    これ意外といけそうやぞ。

--- philo/main.c
+++ philo/main.c
@@ -6,14 +6,33 @@
 /*   By: kei2003730 <kei2003730@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 01:36:25 by kei2003730        #+#    #+#             */
-/*   Updated: 2025/07/11 11:42:39 by kei2003730       ###   ########.fr       */
+/*   Updated: 2025/07/11 13:25:09 by kei2003730       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main(void)
+int main(int argc, char const *argv[])
 {
+	int philo_num;
+	int time_to_die_ms;
+	int time_to_eat_ms;
+	int time_to_sleep_ms;
+	int number_of_times_each_philosopher_must_eat;
+
+	if (argc != 5 && argc != 6)
+	{
+		printf("Your input is wrong.\n");
+		return (1);
+	}
+	philo_num = atoi(argv[1]);
+	time_to_die_ms = atoi(argv[2]);
+	time_to_eat_ms = atoi(argv[3]);
+	time_to_sleep_ms = atoi(argv[4]);
+	if (argc == 6)
+		number_of_times_each_philosopher_must_eat = atoi(argv[5]);
+	
+
 	printf("Hello World!");
 	return (0);
 }
